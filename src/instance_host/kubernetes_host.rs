7e307cb0acf9133321dff63601b40018f02174bc
Mateusz Grzonka <alpinus4@gmail.com>
2023-08-07 21:45:16 +0200

    [WiP] Add list of hosts, connect with kubernetes
    Not working as of now, got some errors, but too tired today to do more

--- src/instance_host/kubernetes_host.rs
+++ src/instance_host/kubernetes_host.rs
@@ -1,16 +1,34 @@
 use crate::instance_host::{Instance, InstanceHost};
 
-pub struct KubernetesHost {}
+use kube::{Client, api::{Api, ResourceExt, ListParams, PostParams}};
+use k8s_openapi::api::core::v1::Pod;
+use async_trait::async_trait;
+
+pub struct KubernetesHost {
+    instances: Vec<Box<Instance>>
+}
 
 impl KubernetesHost {
     pub fn new() -> KubernetesHost {
-        KubernetesHost {}
+        KubernetesHost {
+            instances: Vec::new()
+        }
     }
 }
 
+#[async_trait]
 impl InstanceHost for KubernetesHost {
-    fn start_instance(&self) -> Instance {
-        todo!()
+    async fn start_instance(&mut self) -> Result<Instance, Box<dyn std::error::Error>> {
+        let client = Client::try_default().await?;
+
+        let pods: Api<Pod> = Api::default_namespaced(client);
+        for p in pods.list(&ListParams::default()).await? {
+            println!("found pod {}", p.name_any());
+        }
+
+        let instance = Instance::new(String::from("xd"), 9000);
+        self.instances.push(Box::new(instance.clone()));
+        Ok(instance)
     }
 
     fn stop_instance(&self, instance: Instance) {
--- src/instance_host/mod.rs
+++ src/instance_host/mod.rs
@@ -1,15 +1,25 @@
 pub mod kubernetes_host;
 
+use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Instance {
     url: String,
     port: u16,
 }
 
+impl Instance {
+    pub fn new(url: String, port: u16) -> Instance {
+        Instance {
+            url, port
+        }
+    }
+}
+
+#[async_trait]
 pub trait InstanceHost {
-    fn start_instance(&self) -> Instance;
+    async fn start_instance(&mut self) -> Result<Instance, Box<dyn std::error::Error>>;
 
     fn stop_instance(&self, instance: Instance);
 }
