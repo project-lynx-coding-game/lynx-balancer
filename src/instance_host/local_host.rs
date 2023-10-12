e3f668b1d03cbd77b245b0d196a416688893a6c2
Mateusz Grzonka <alpinus4@gmail.com>
2023-10-12 23:27:58 +0200

    Run scene host through podman in local instance host

--- src/instance_host/local_host.rs
+++ src/instance_host/local_host.rs
@@ -1,6 +1,7 @@
 use crate::instance_host::{Instance, InstanceHost};
 
 use async_trait::async_trait;
+use std::net::TcpListener;
 use std::process::{Command, Child};
 use tracing::info;
 use std::collections::HashMap;
@@ -17,20 +18,33 @@ impl LocalHost {
     }
 }
 
+fn get_available_port() -> Option<u16> {
+    (8000..9000)
+        .find(|port| port_is_available(*port))
+}
+
+fn port_is_available(port: u16) -> bool {
+    match TcpListener::bind(("127.0.0.1", port)) {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
+
 #[async_trait]
 impl InstanceHost for LocalHost {
     async fn start_instance(
         &mut self,
         username: String,
     ) -> Result<Instance, Box<dyn std::error::Error>> {
-        // TODO: argument providing path to python entrypoint, execute
+        // TODO: if existing user, first kill previous
+        let port = get_available_port().expect("no available ports");
         let child = Command::new("sh")
                 .arg("-c")
-                .arg("echo hello && sleep 30")
+                .arg(format!("podman run -p {}:{} ghcr.io/group-project-gut/lynx-scene-host-python:latest main:app --port {} --host 0.0.0.0 --workers 1", port, port, port))
                 .spawn()
                 .expect("failed to execute process");
         self.processes.insert(username.clone(), child);
-        let instance = Instance::new("0.0.0.0".to_string(), 8555);
+        let instance = Instance::new("0.0.0.0".to_string(), port);
         Ok(instance)
     }
 
